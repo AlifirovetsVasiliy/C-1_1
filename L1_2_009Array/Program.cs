77f0deeef8befbb09a26e9eeb3503a936608ceb8
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-09 13:38:32 +0500

    задача по лекции 009 массив

---
+++ L1_2_009Array/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 19, 28, 37, 64, 505, 46, 37, 82, 91 };
+
+int result1 = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result1);
