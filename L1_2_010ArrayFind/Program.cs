fcf5fb8e311ce5dbdda5d0781e29c6195ed25047
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-09 14:27:06 +0500

    задача по лекции 010 массив поиск

---
+++ L1_2_010ArrayFind/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = { 19, 28, 37,91, 64, 505, 46, 37, 82, 91 };
+
+int n = array.Length;
+int find = 91;
+
+int index = 0;
+
+while (index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
