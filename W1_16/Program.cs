b2c8df2f94b64c5a8850e8d132ac73bb44be77ce
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-09 18:50:03 +0500

    задача 16 является ли квадратом

---
+++ W1_16/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два
+// числа и проверяет, является ли одно число квадратом
+// другого.
+
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Clear();
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b * b == a || a * a == b)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
