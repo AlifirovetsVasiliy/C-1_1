4a22c1637362dd12d7d08e2ac72ea1223649373c
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-09 18:31:57 +0500

    задача 13 тертья цифра заданного числа

---
+++ W1_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    int b = a / 100;
+    int c = b % 10;
+    Console.Write(c);
+}
