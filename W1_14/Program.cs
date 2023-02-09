1f434aa670d0460fa6738c82b341d330738ded1a
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-09 18:05:15 +0500

    задача 14 кратно 7 и 23

---
+++ W1_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход
+//число и проверяет, кратно ли оно одновременно
+//7 и 23.
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 7;
+int c = 23;
+int d = a % b;
+int e = a % c;
+if (d == 0 & e == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
