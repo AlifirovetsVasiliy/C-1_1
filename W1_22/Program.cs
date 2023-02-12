a5b0b28bb17cfa5104125be1ebc936612c254905
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-13 00:16:54 +0500

    До n в квадрате

---
+++ W1_22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count <= N)
+{
+    double result = Math.Pow(count, 2);
+    Console.Write($"{result}");
+    if (count < N) Console.Write(", ");
+    else Console.Write(".");
+    count++;
+}
