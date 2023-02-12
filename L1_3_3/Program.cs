9d71eb20916eca55985c05638a23fd483a87634a
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-12 17:36:18 +0500

    упорядочивание от минимального

---
+++ L1_3_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Упорядочить массивы
+
+Console.Clear();
+
+int[] arr = {1,4,7,8,5,2,3,6,9,1,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+     for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
