8de23ab54a4d9bc6aba191a402cdb72fee6928f6
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-13 21:41:28 +0500

    №27 на вход число и выдаёт сумму цифр в числе.

---
+++ W1_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int ReSum (int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res += n % 10;
+        n = n / 10;
+    }
+    return res;
+}
+
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма всех чисел в: {N} равна: {ReSum(N)}");
