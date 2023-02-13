938370a73707e206b83e478d3e4ebb3fe7ed4982
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-13 20:03:53 +0500

    №24 сумма чисел до А

---
+++ W1_24/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+Console.Write("Введите  A: ");
+int N = int.Parse(Console.ReadLine()!);
+// int Sum = 0;
+// for (int n = 1; n <= A; n++)
+// {
+//     Sum = Sum + n;
+// }
+//     Console.WriteLine($"{A} = {Sum}");
+
+int GetSum (int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum +=i; // sum = sum + i
+    }
+    return sum;
+}
+ Console.WriteLine($"Сумма чисел от 1 до {N} равна {GetSum(N)}");
