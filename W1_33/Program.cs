f84be2f09bcbfc23c3eb28adb03dc9d70d681e14
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-17 18:41:05 +0500

    №33 Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.

---
+++ W1_33/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+bool FindElement(int number, int[] collection)
+{
+    foreach (int el in collection)
+    {
+        if (el == number)
+        {
+            return true;
+        }
+    }
+    return false;
+
+
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (FindElement(n, array))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
