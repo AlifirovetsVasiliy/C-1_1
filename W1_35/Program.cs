149a2b1401569a59c9c5272fc85c180ca0438b14
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-17 22:02:53 +0500

    №35  одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]

---
+++ W1_35/Program.cs
@@ -0,0 +1,36 @@
+﻿//  одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99]. 
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int printArray(int[] inArray, int minNumber, int maxNumber)
+{
+    int resultNum = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] >= minNumber && inArray[i] <= maxNumber)
+        {
+            resultNum += 1;
+        }
+    }
+    return resultNum;
+}
+
+int[] array = GetArray(123, 0, 300);
+Console.WriteLine(string.Join(", ", array));
+int resultN = printArray(array, 10, 99);
+Console.WriteLine($"Колличество чисел в диапозоне значений от 10 до 99  равно {resultN}");
+
