d641002fb409445fe7ffc302719b977306ac6265
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-17 22:46:50 +0500

    37 Найдите произведение пар чисел в одномерном массиве.

---
+++ W1_37/Program.cs
@@ -0,0 +1,35 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+int[] MultArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 != 0) size++;
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 != 0) result[size - 1] = array[array.Length / 2];
+    return result;
+}
+int[] inArray = GetArray(4, -9, 9);
+Console.WriteLine(string.Join(", ", inArray));
+int[] newArray = MultArray(inArray);
+Console.WriteLine(string.Join(", ", newArray));
