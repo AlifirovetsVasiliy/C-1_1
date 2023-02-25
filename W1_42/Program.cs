d6b9d28ffa753058f82dc3d8c35ec7579032558c
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-25 18:16:30 +0300

    42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.

---
+++ W1_42/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string ConvertNumber(int num)
+{
+    string result = String.Empty;
+    while (num != 0)
+    {
+        result = (num % 2).ToString() + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ConvertNumber(n));
