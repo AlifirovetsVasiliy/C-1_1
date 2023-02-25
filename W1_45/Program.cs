1bc09c3e52ad8a725c6460a99f2fb13e947174cd
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-02-25 19:02:53 +0300

    45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ W1_45/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int[] CloneArray(int[] inArray)
+{
+    int[] newArr = new int [inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        newArr[i] = inArray[i];
+    }
+    return newArr;
+}
+
+Console.Clear();
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+int[] clone = CloneArray(array);
+Console.WriteLine("Clone: " + String.Join(", ", clone));
