faf4ac67406d6c55396033b50b8a19fdad9230cb
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-03-01 13:20:16 +0300

    Просмотр содиржимого в папках

---
+++ L1_7_2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Console.Clear();
+// string path = "D:/git_AlifirovetsV2/С#1_1/L1_1/L1_2_009Array";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+Console.Clear();
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = @"D:/git_AlifirovetsV2/С#1_1/L1_1/L1_2_1";
+CatalogInfo(path);
