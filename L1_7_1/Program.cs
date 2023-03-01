361b9fa6d73ca770615cbd4b94ee3708af673c79
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-03-01 12:45:03 +0300

    7_1 В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из T букв, которые можно построить из букв этого алфавита

---
+++ L1_7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// у нас будет некоторый алфавит, состоящий из
+// четырёх букв, и нас просят показать все возможные слова, состоящие из T букв. Т может
+// равняться 1, 2, 3 и так далее.
+Console.Clear();
+int n= 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+if (length == word.Length)
+{
+Console.WriteLine($"{n++} {new String(word)}"); return;
+}
+for (int i = 0; i < alphabet.Length; i++)
+{
+word[length] = alphabet[i];
+FindWords(alphabet, word, length + 1);
+}
+}
+FindWords("аисв", new char[3]);
