e9384b469bd5d668ef2026cb723abba16c7f6939
AlifirovetsVasiliy <fraddy891@gmail.com>
2023-03-09 21:29:45 +0300

    54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.

---
+++ W1_54/Program.cs
@@ -0,0 +1,64 @@
+﻿// 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void Sort(int[,] array)
+{
+       for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++) 
+        {
+            for (int m = j + 1; m < array.GetLength(1); m++)
+            {
+                if (array[i, j] < array[i, m])
+                {
+                        int temp = array[i, j];
+                        array[i, j] = array[i, m];
+                        array[i, m] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+Console.Clear();
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array2D = GetArray(row, col, 0, 10);
+PrintArray(array2D);
+
+Console.WriteLine();
+Sort(array2D);
+PrintArray(array2D);
